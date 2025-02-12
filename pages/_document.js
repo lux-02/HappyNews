251ba537a2931152d3d619298c3cecffcd319345
lux00 <oys128950@gmail.com>
2025-02-12 14:03:41 +0900

    apply to google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FC7VTX0SPS"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FC7VTX0SPS');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
